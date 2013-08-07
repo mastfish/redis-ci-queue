be3536e6dc50bb5dde7f15528f38fa25b1ee2ec0
Justin Lambert <justin.lambert@bigcommerce.com>
2013-08-07 16:23:11 +1000

    dump everything in the queue and go for beer

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@ require 'json'
 post '/ci' do
   uri = URI.parse(ENV["REDISTOGO_URL"])
   redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  data = {this: true}
-  redis.publish 'orchestra:builds', data.to_json
+  redis.publish 'orchestra:builds', params.to_json
   "data published"
 end
