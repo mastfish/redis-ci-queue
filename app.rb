ed6f01884dd2adad56ec9c4fc4d826445b12b9af
Justin Lambert <justin.lambert@bigcommerce.com>
2013-08-07 16:31:16 +1000

    get request payload

--- app.rb
+++ app.rb
@@ -6,6 +6,6 @@ require 'json'
 post '/ci' do
   uri = URI.parse(ENV["REDISTOGO_URL"])
   redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  redis.publish 'orchestra:builds', 'hi tim'
+  redis.publish 'orchestra:builds', request.body.read
   "data published"
 end
