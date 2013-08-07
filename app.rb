e65db96d8f76e59f4ddf275c3a201f37ad23f8c0
Justin Lambert <justin.lambert@bigcommerce.com>
2013-08-07 16:24:44 +1000

    add test string

--- app.rb
+++ app.rb
@@ -6,6 +6,6 @@ require 'json'
 post '/ci' do
   uri = URI.parse(ENV["REDISTOGO_URL"])
   redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  redis.publish 'orchestra:builds', params.to_json
+  redis.publish 'orchestra:builds', 'hi tim'
   "data published"
 end
